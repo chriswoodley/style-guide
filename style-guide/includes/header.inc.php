6065ac3341f67f877d70c8eac21d0ef6e5bffa56
Chris Woodley <chriswoodley@me.com>
2014-06-29 10:10:16 -0700

    header: add mobile meta tags

--- style-guide/includes/header.inc.php
+++ style-guide/includes/header.inc.php
@@ -3,6 +3,12 @@
 <head>
     <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
     <title>Styleguide Example</title>
+    <meta name="description" content="">
+    <meta name="HandheldFriendly" content="True">
+    <meta name="MobileOptimized" content="320">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui">
+    <meta http-equiv="cleartype" content="on">
+    <script src="/bower_components/html5shiv/dist/html5shiv.min.js"></script>
     <link rel="stylesheet" href="css/styleguide.css" />
     <link rel="stylesheet" href="css/style.css" />
 </head>
