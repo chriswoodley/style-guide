e1ac693369e8865eec8a071c742faa41971e005b
Chris Woodley <chriswoodley@me.com>
2014-07-19 09:56:53 -0700

    includes: add html5shiv, nwmatcher, selectivizr, and respond to the head

--- style-guide/includes/header.inc.php
+++ style-guide/includes/header.inc.php
@@ -8,9 +8,14 @@
     <meta name="MobileOptimized" content="320">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui">
     <meta http-equiv="cleartype" content="on">
-    <script src="/bower_components/html5shiv/dist/html5shiv.min.js"></script>
     <link rel="stylesheet" href="css/styleguide.css" />
     <link rel="stylesheet" href="css/style.css" />
+    <!--[if lt IE 9]>
+    <script src="/bower_components/html5shiv/dist/html5shiv.min.js"></script>
+    <script src="/bower_components/nwmatcher/src/nwmatcher.js"></script>
+    <script src="/bower_components/selectivizr/selectivizr.js"></script>
+    <script src="/bower_components/respond/dest/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
 
